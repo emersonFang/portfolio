47e0f7a65864669fcc21a5737bdfc38d4d5cde61
Emerson Fang <efang@seas.harvard.edu>
2015-09-16 04:26:51 -0400

    more editing of p1.php, added styling

--- p1/p1.php
+++ p1/p1.php
@@ -4,19 +4,45 @@
 <head>
 	<meta charset="utf-8">
 	<title>Emerson Fang DWA-15 P1 - Portfolio</title>
-	<link rel="stylesheet" href="../style.css" />
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 	<style>
-	
-		h1 {
+
+		h1{
 			text-align:center;
 		}
-		
+
+		nav {
+			text-align: center;
+		}
+
+		.container {
+			width:75%;
+			min-width:800px;
+			margin-bottom:50px;
+		}
+
+		.inner {
+			margin-left: 10%;
+		}
+
+		ul {
+		    list-style-type: none;
+		    margin: 0;
+		    padding: 0;
+		    display: inline;
+		}
+
+		li {
+ 		    display: inline-table;
+		}
+
 		img {
 			display:block;
 			margin:auto;
-			width:50%;
-			height:50%;
+			width:25%;
+			height:25%;
 		}
 	
 	</style>
@@ -24,35 +50,40 @@
 
 <body>
 
+<div class="container">
+
 	<header role="banner">
-		<h1>Emerson Fang's Dynamic Web Apps Portfolio</h1>
+		<h1>Emerson Fang's Dynamic Web Apps Portfolio</h1>	
 			<img src="../images/emerson.png" alt="Emerson's Face" class="profile">
 			<nav role="navigation">
 			<ul>
-				<li>Project 1</li>
+				<li><h3>Project 1</h3></li>
 					<ul>
-						<li><a href="p1.php" title="Emerson's DWA-15 P1">View P1</a></li>
+						<li><a href="p1.php" title="Emerson's DWA-15 P1">View</a></li>
 						<li><a href="" title="P1 Github Link">Github</a></li>
 					</ul>
-				<li>Project 2</li>
+				<li><h3>Project 2</h3></li>
 					<ul>
-						<li><a href="p2.php" title="Emerson's DWA-15 P2">View P2</a></li>
+						<li><a href="p2.php" title="Emerson's DWA-15 P2">View</a></li>
 						<li><a href="" title="P2 Github Link">Github</a></li>
 					</ul>
-				<li>Project 3</li>
+				<li><h3>Project 3</h3></li>
 					<ul>
-						<li><a href="p3.php" title="Emerson's DWA-15 P3">View P3</a></li>
+						<li><a href="p3.php" title="Emerson's DWA-15 P3">View</a></li>
 						<li><a href="" title="P3 Github Link">Github</a></li>
 					</ul>
-				<li>Project 4</li>
+				<li><h3>Project 4</h3></li>
 					<ul>
-						<li><a href="p4.php" title="Emerson's DWA-15 P4">View P4</a></li>
+						<li><a href="p4.php" title="Emerson's DWA-15 P4">View</a></li>
 						<li><a href="" title="P4 Github Link">Github</a></li>
 					</ul>
 			</ul>
 			</nav>
 	</header>
 
+	<br>
+
+<div class="inner">
 	<h2>In 2+ sentences describe who you are and what you do.</h2>
 		<p>I am a mild-mannered guy.  By day, I am a research assistant in a science lab.  By night, I am a caped-crusader.  Just kidding.</p>
 		
@@ -60,13 +91,18 @@
 		<p>I am taking this course for self-enrichment, and for future career endeavors.</p>
 		
 	<h2>What previous experience do you have with programming?</h2>
-		<p>Java, SQL, Linux, Matlab, some C++</p>
+		<p>Java, SQL, Linux, Matlab, some C++, Coldfusion.  As far as web design, I've watched basic HTML/CSS Lynda.com videos.</p>
 		
 	<h2>Are you working on a Mac, PC, or some other platform?</h2>
 		<p>PC</p>
 		
 	<h2>Anything else we should know?</h2>
-		<p>If you look in my backpack, you will not find a cowl.</p>	
+		<p>If you look in my backpack, you will not find a cowl.</p>
+</div>
+
+</div>
+
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </body>
 </html>
